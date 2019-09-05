906da6c422d675d782127bd8cefee3a065bd0520
Masahiko Suda <suda@grafz.net>
2019-09-05 20:50:24 +0900

    interactive mode

--- manual.py
+++ manual.py
@@ -1,8 +1,6 @@
 # manual.py
 #
 # this script will be used for manipulating motor angles manually
-# usage syntax: python manual.py [deg_motor1] [deg_motor2] [deg_motor3]
-# usage example: python manual.py 60 30 -30
 # LICENSE: MIT License
 # (C) Seltec Lab 2019
 
@@ -21,7 +19,8 @@ PIN_STEP_MOT3 = 19
 # NEMA17
 STEP_RESOLUTION = 1
 DEG_PER_STEP = 1.8 / STEP_RESOLUTION
-DELAY_PER_STEP = 0.006 # 100RPM
+#DELAY_PER_STEP = 0.006 # 100RPM
+DELAY_PER_STEP = 0.048 # 12.5RPM
 
 # other constants
 SPR = 360 / DEG_PER_STEP
@@ -38,10 +37,6 @@ def arg_to_steps(arg):
     direction = CCW if raw_angle < 0 else CW
     return (steps, direction)
 
-(s_m1, d_m1) = arg_to_steps(sys.argv[1])
-(s_m2, d_m2) = arg_to_steps(sys.argv[2])
-(s_m3, d_m3) = arg_to_steps(sys.argv[3])
-
 def biggest(a, y, z):
     Max = a
     if y > Max:
@@ -51,11 +46,6 @@ def biggest(a, y, z):
         if y > z:
             Max = y
     return Max
-
-max_step = biggest(s_m1, s_m2, s_m3)
-span_m1 = 0.5 * DELAY_PER_STEP * max_step / s_m1
-span_m2 = 0.5 * DELAY_PER_STEP * max_step / s_m2
-span_m3 = 0.5 * DELAY_PER_STEP * max_step / s_m3
 
 # setting up GPIO pins
 GPIO.setmode(GPIO.BCM)
@@ -76,6 +66,9 @@ GPIO.output(PIN_STEP_MOT3, GPIO.LOW)
 
 # rotate function
 def rotate(steps, direction, pin_dir, pin_step, span_delay):
+    if not span_delay or not steps:
+        return
+
     GPIO.output(pin_dir, direction)
     for x in range(steps):
         GPIO.output(pin_step, GPIO.HIGH)
@@ -83,26 +76,54 @@ def rotate(steps, direction, pin_dir, pin_step, span_delay):
         GPIO.output(pin_step, GPIO.LOW)
         sleep(span_delay)
 
-# execute rotation using threading
-pin_direc = PIN_DIR_MOT1
-pin_step = PIN_STEP_MOT1
-t1 = threading.Thread(target=rotate, args=(s_m1, d_m1, pin_direc, pin_step, span_m1))
-pin_direc = PIN_DIR_MOT2
-pin_step = PIN_STEP_MOT2
-t2 = threading.Thread(target=rotate, args=(s_m2, d_m2, pin_direc, pin_step, span_m2))
-pin_direc = PIN_DIR_MOT3
-pin_step = PIN_STEP_MOT3
-t3 = threading.Thread(target=rotate, args=(s_m3, d_m3, pin_direc, pin_step, span_m3))
+def get_span_params(s_m1, s_m2, s_m3):
+    max_step = biggest(s_m1, s_m2, s_m3)
+    try:
+        span_m1 = 0.5 * DELAY_PER_STEP * max_step / s_m1
+    except:
+        span_m1 = None
+    try:
+        span_m2 = 0.5 * DELAY_PER_STEP * max_step / s_m2
+    except:
+        span_m2 = None
+    try:
+        span_m3 = 0.5 * DELAY_PER_STEP * max_step / s_m3
+    except:
+        span_m3 = None
+    return (span_m1, span_m2, span_m3)
 
-# start execution
-t1.start()
-t2.start()
-t3.start()
+try:
+    while True:
+        deg1 = input('motor1 angle:')
+        deg2 = input('motor2 angle:')
+        deg3 = input('motor3 angle:')
+        (s_m1, d_m1) = arg_to_steps(deg1)
+        (s_m2, d_m2) = arg_to_steps(deg2)
+        (s_m3, d_m3) = arg_to_steps(deg3)
+        (span_m1, span_m2, span_m3) = get_span_params(s_m1, s_m2, s_m3)
 
-# wait until all motors finish rotation
-t1.join()
-t2.join()
-t3.join()
+        # execute rotation using threading
+        pin_direc = PIN_DIR_MOT1
+        pin_step = PIN_STEP_MOT1
+        t1 = threading.Thread(target=rotate, args=(s_m1, d_m1, pin_direc, pin_step, span_m1))
+        pin_direc = PIN_DIR_MOT2
+        pin_step = PIN_STEP_MOT2
+        t2 = threading.Thread(target=rotate, args=(s_m2, d_m2, pin_direc, pin_step, span_m2))
+        pin_direc = PIN_DIR_MOT3
+        pin_step = PIN_STEP_MOT3
+        t3 = threading.Thread(target=rotate, args=(s_m3, d_m3, pin_direc, pin_step, span_m3))
 
-GPIO.cleanup()
+        # start execution
+        t1.start()
+        t2.start()
+        t3.start()
+
+        # wait until all motors finish rotation
+        t1.join()
+        t2.join()
+        t3.join()
+except KeyboardInterrupt:
+    print('\nterminating...')
+    GPIO.cleanup()
+    sys.exit()
 
